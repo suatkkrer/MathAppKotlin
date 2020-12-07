c5e3d19ac73ae7be48415a573b1a2852426e4252
suatkkrer <suat.kukrer@gmail.com>
2020-12-07 10:08:50 +0100

    new Activity Added

--- app/src/main/java/com/suatkkrer/mathappkotlin/AdditionActivity.kt
+++ app/src/main/java/com/suatkkrer/mathappkotlin/AdditionActivity.kt
@@ -3,14 +3,9 @@ package com.suatkkrer.mathappkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AdditionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
-
-
+        setContentView(R.layout.activity_addition)
     }
 }
